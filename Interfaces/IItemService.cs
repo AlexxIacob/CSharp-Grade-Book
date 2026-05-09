c906e1a9e1742390adbc345eb4a7ce1dec650329
AlexxIacob <alexandruiacob01@gmail.com>
2026-05-09 19:23:38 +0300

    fix: added ItemService for handling bussiness logic

---
+++ Interfaces/IItemService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Siemens.Internship2026.GradeBook.Models;
+
+namespace Siemens.Internship2026.GradeBook.Interfaces;
+
+public interface IItemService
+{
+    Task<IEnumerable<Item>> GetAllAsync();
+    Task<Item?> GetByIdAsync(int id);
+}
+
---
+++ Services/ItemService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Siemens.Internship2026.Gradebook.Models;
+using Siemens.Internship2026.Gradebook.Interfaces;
+
+public class ItemService : IItemService
+{
+    private readonly IItemReader _reader;
+
+    public ItemService (IItemService reader)
+    {
+        _reader = reader;
+    }
+
+    public Task<IEnumerable<Item>> GetAllAsync()
+    {
+        return _reader.GetAllAsync();
+    }
+
+    public Task<Item?> GetByIdAsync(int id)
+    {
+        return _reader.GetByIdAsync(id);
+    }
+}
