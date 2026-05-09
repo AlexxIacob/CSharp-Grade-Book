52116e9e82e009320dd46f71a4ab0f0dea96c924
AlexxIacob <alexandruiacob01@gmail.com>
2026-05-09 20:43:39 +0300

    nothing added

--- Interfaces/IItemReader.cs
+++ Interfaces/IItemReader.cs
@@ -2,6 +2,7 @@ using Siemens.Internship2026.GradeBook.Models;
 
 namespace Siemens.Internship2026.GradeBook.Interfaces;
 
+
 public interface IItemReader
 {
     Task<Item?> GetByIdAsync(int id);
--- Models/Item.cs
+++ Models/Item.cs
@@ -1,5 +1,7 @@
 namespace Siemens.Internship2026.GradeBook.Models;
 
+
+
 public class Item
 {
     public int Id { get; set; }
