436034dcd35b43514d35c623104983cbe8fd982a
AlexxIacob <alexandruiacob01@gmail.com>
2026-05-09 19:58:03 +0300

    fix: register IItemReader and IItemService in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using Siemens.Internship2026.GradeBook.Interfaces;
+using Siemens.Internship2026.GradeBook.Repositories;
+using Siemens.Internship2026.GradeBook.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<IItemReader, HttpItemRepository>();
+builder.Services.AddScoped<IItemService, ItemService>();
 
 var app = builder.Build();
+
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
